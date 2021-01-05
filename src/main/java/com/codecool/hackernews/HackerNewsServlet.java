d74c1714617f3b7b65a319a69fdeafc22aaba13e
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-01-06 00:35:49 +0100

    newest news web page work

--- src/main/java/com/codecool/hackernews/HackerNewsServlet.java
+++ src/main/java/com/codecool/hackernews/HackerNewsServlet.java
@@ -1,6 +1,7 @@
 package com.codecool.hackernews;
 
 import com.codecool.hackernews.common.Const;
+import com.codecool.hackernews.common.DataType;
 import com.codecool.hackernews.dao.NewsDao;
 import com.codecool.hackernews.models.NewsModel;
 import com.codecool.hackernews.templates.NewsTemplate;
@@ -15,19 +16,29 @@ import java.util.List;
 /**
  * Supports news pages.
  */
-@WebServlet(name = "hackerNewsServlet", urlPatterns = {"", "/top"}, loadOnStartup = 1)
+@WebServlet(name = "hackerNewsServlet", urlPatterns = {"", "/top", "/newest"}, loadOnStartup = 1)
 public class HackerNewsServlet extends javax.servlet.http.HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
+        String dataType = getDataType(request.getServletPath());
         String pageNumber = request.getParameter("page");
 
-        List<NewsModel> news = new NewsDao(Const.DataType.TOP, pageNumber).getNews();
+        List<NewsModel> news = new NewsDao(dataType, pageNumber).getNews();
         String pageTemplate = new NewsTemplate("", pageNumber, news).getTemplate();
 
         PrintWriter out = response.getWriter();
         out.println(pageTemplate);
+    }
+
+    /* Returns the news type based on the servlet path. */
+    private String getDataType(String servletPath) {
+        String dataType = DataType.getType(servletPath);
+        if (dataType == null)  // default page
+            dataType = Const.DataType.TOP;
+
+        return dataType;
     }
 }
